69eb54ed90179831f8a489e8baabd562ab47418a
André Marín <andre@abk.de>
2025-10-07 11:41:07 +0200

    Create for loop sample

---
+++ b_for/ForSchleife.java
@@ -0,0 +1,15 @@
+public class ForSchleife
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 20; i++)
+        {
+            System.out.println("Hallo " + i);
+        }
+
+    }
+}
+
+
+
+
