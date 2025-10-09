5c8b169790a9d2d500acc6661e17516fa5bde26b
André Marín <andre@abk.de>
2025-10-09 14:22:54 +0200

    Add new sample with errors

---
+++ g_geschachteltes_if/ABCAnalyse2.java
@@ -0,0 +1,40 @@
+// b05_kontrollstruktur
+// g_geschachteltes_if
+
+// Schlechter Code
+public class ABCAnalyse2
+{
+  public static void main (String[] args)
+  {
+    char prioritaet = 'B';
+
+    if(prioritaet == 'A')
+    {
+      System.out.println("Hoch");
+    }
+    else
+    {
+    }
+      if(prioritaet == 'B')
+      {
+        System.out.println("Mittel");
+      }
+      else
+      {
+        if(prioritaet == 'C')
+        {
+          System.out.println("Niedrig");
+        }
+        else
+        {
+          System.out.println("Unzulaessig");
+        }
+      }
+  }
+}
+
+
+
+
+
+
